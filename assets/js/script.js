359753b68ee6e2e8bbb9e8026ce8ca38ab6ff11f
Guillaume Baudusseau <guillaume.baudusseau@gmail.com>
2016-04-22 11:27:10 -0400

    activate login button if inputs are filled

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+(function() {
+
+    var username =  document.getElementById('username-field');
+    var password = document.getElementById('password-field');
+    var loginBtn = document.getElementById('login-btn');
+
+
+    username.addEventListener("input", function() {
+        checkForm();
+    }, true);
+    password.addEventListener("input", function() {
+        checkForm();
+        console.log('hello');
+    }, true);
+
+    function checkForm() {
+        if(username.value && password.value && loginBtn.className != 'active') {
+            loginBtn.className += 'active';
+        } else if (!username.value || !password.value) {
+            loginBtn.className = '';
+        }
+    }
+
+})();
