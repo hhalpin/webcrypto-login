251e57fe96a1dc5446dfa2dc85ac9ff639c2a728
Guillaume Baudusseau <guillaume.baudusseau@gmail.com>
2016-04-22 16:58:14 -0400

    add rotate animation within login btn

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,6 +18,11 @@
         if(loginBtn.className === 'active') {
             loginBtn.innerHTML = '';
             loginBtn.className += ' loading';
+            loginBtn.innerHTML = '<?xml version="1.0"?>'
+            + '<svg viewBox="0 0 120 120" version="1.1" xmlns="http://www.w3.org/2000/svg">'
+            + '<circle cx="60" cy="60" r="35"  stroke="rgba(255,255,255, 0.4)" stroke-width="5" fill=none />'
+            + '<circle id="hilighted-circle" cx="60" cy="60" r="35" stroke="rgba(255,255,255, 1)" stroke-width="5" fill=none />'
+            + '</svg>'
             forgotPassword.className += ' hidden';
         }
     }, true);
